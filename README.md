d7d3cbf77828f01037c5f3465e2bb85e8ca456c3
Josh Toth <toth.jmt@gmail.com>
2015-11-23 21:03:43 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Photoshop Canvas Print Generator
+
+Easily set up ready-to-print canvas documents with a few quick prompts.
+
+##Use
+Download the `new-canvas.jsx` file somewhere to your computer and double-click it. On some systems, you may need to drag it to a running instance of Photoshop.
+
+##Prompts
+
+The script will ask you several questions to set up your document:
+
+- **New Document Name?**
+	- Doesn't actually affect the end product; simply for housekeeping. Will be the default filename when you save.
+- **Document Width?**
+	- Width of *front* of canvas, in inches.
+- **Document Height?**
+	- Height of *front* of canvas, in inches.
+- **Stretcher Bar Width?**
+	- How thick the frame is (depth of canvas), in inches.
+- **Buffer Amount?**
+	- How much canvas material you want to wrap around to the back (the amount you'll have left to staple).
+
+
+## Adding your image
+After following the prompts, click `File` > `Place Embedded` or `Place Linked` (your choice of which. If you don't know the difference, the two will act the same for what you need).
+
+## The Layers
+- **Cut Line**
+	- Not visible by default. Optional to print. I added this for two reasons:
+		- When printing on larger-than-your-image canvases, it's difficult to tell where to cut, if you have any white space left over
+		- I tend to have a space-saver option turned on with my printer and it will crop it *right* to the image by default; this forces it to print the whole thing
+- **Overlay**
+	- Not visible by default. Simply a reference layer (**don't** print with it visible!). Allows you to visualize what will be visible on the front of the canvas.
+- **Background**
+	- Just a white background.
+
+## Notes
+- You don't have to fill the *entire* document with your image - as long as you make it a little past the outermost guidelines, the image will fill the front and sides of the canvas. If you're okay with some blank canvas on the back (I usually am), you will be able to get more of your image on the front.
+- In the "Buffer Amount" question - I recommend to add `.25` or `.5` more than you think you need - canvas going around the corners always seems to steal from my actual dimensions. You can always cut more away, but you can't get extra put on!
---
+++ new-canvas.jsx
@@ -0,0 +1,111 @@
+﻿var devMode = false;
+
+if (devMode)
+{
+	var documentName = 'Dev Doc';
+	var artWidth = 20;
+	var artHeight = 16;
+	var barWidth = 1;
+	var buffer = 1;
+
+}
+else
+{
+	var documentName = prompt('New Document Name?', 'New Doc');
+	var artWidth = Number(prompt('Document Width?', 20)) || 20;
+	var artHeight = Number(prompt('Document Height?', 16)) || 16;
+	var barWidth = Number(prompt('Stretcher Bar Width?', 1));
+	var buffer = Number(prompt('Buffer Amount (Length that should fold onto back)?', 1)) || 1;
+}
+
+
+
+var docResolution = 300;
+
+var docWidth = artWidth + (barWidth * 2) + 2;
+var docHeight = artHeight + (barWidth * 2) + 2;
+
+
+var docWidth = artWidth + (barWidth * 2) + (buffer * 2);
+var docHeight = artHeight + (barWidth * 2)+ (buffer * 2);
+
+
+app.preferences.rulerUnits = Units.INCHES;
+
+app.documents.add(docWidth, docHeight, docResolution, documentName , NewDocumentMode.CMYK, DocumentFill.WHITE);
+
+
+// Create Guides for back of canvas
+app.activeDocument.guides.add(Direction.HORIZONTAL, buffer);
+app.activeDocument.guides.add(Direction.HORIZONTAL, docHeight - buffer);
+app.activeDocument.guides.add(Direction.VERTICAL,buffer);
+app.activeDocument.guides.add(Direction.VERTICAL, docWidth - buffer);
+
+
+
+// Create Guides for sides
+app.activeDocument.guides.add(Direction.HORIZONTAL, buffer + barWidth);
+app.activeDocument.guides.add(Direction.HORIZONTAL, docHeight - buffer - barWidth);
+app.activeDocument.guides.add(Direction.VERTICAL, buffer + barWidth);
+app.activeDocument.guides.add(Direction.VERTICAL, docWidth - buffer - barWidth);
+
+
+
+// Add overlay
+var overlay = app.activeDocument.artLayers.add();
+overlay.name = 'Overlay';
+overlay.opacity = 95;
+
+
+var selectionBoundaries = [
+	[buffer + barWidth, buffer + barWidth],
+	[docWidth - buffer - barWidth, buffer + barWidth],
+	[docWidth - buffer - barWidth, docHeight - buffer - barWidth],
+	[buffer + barWidth, docHeight - buffer - barWidth]
+];
+ 
+var finalOverlaySelection = [];
+for (i = 0; i < selectionBoundaries.length; i++)
+{
+    finalOverlaySelection[i] = [ (selectionBoundaries[i][0] * docResolution), (selectionBoundaries[i][1] * docResolution) ];
+}
+                   
+app.activeDocument.selection.select(finalOverlaySelection);
+app.activeDocument.selection.invert();
+
+var black = new CMYKColor;
+    black.black = 100;
+    black.cyan = 0;
+    black.magenta = 0;
+    black.yellow = 0;
+
+app.activeDocument.selection.fill(black);
+app.activeDocument.selection.deselect();
+overlay.visible = false;
+
+
+
+// Create cut line
+var cut = app.activeDocument.artLayers.add();
+cut.name = 'Cut line';
+cut.opacity = 50;
+
+app.activeDocument.selection.selectAll();
+app.activeDocument.selection.contract(2);
+app.activeDocument.selection.invert();
+
+var grey = new CMYKColor;
+    grey.black = 50;
+    grey.cyan = 0;
+    grey.magenta = 0;
+    grey.yellow = 0;
+
+app.activeDocument.selection.fill(grey);
+app.activeDocument.selection.deselect();
+cut.visible = false;
+
+
+
+// Restore to normal setup
+var background = app.activeDocument.layers.getByName('Background');
+app.activeDocument.activeLayer = background;
